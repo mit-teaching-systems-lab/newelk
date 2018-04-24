54fac186902e9d36c77453b242c5d82d14e04e10
Brandon <bhanks@mit.edu>
2018-04-24 18:25:03 -0400

    troubleshooting static

--- elk/settings.py
+++ elk/settings.py
@@ -156,12 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+import django_heroku
+django_heroku.settings(locals())
