efe1131bf63599c68a0bc01e884a448dd6469084
Brandon <bhanks@mit.edu>
2018-04-30 15:56:51 -0400

    prep for logins & potential model rewrite

--- chat/models.py
+++ chat/models.py
@@ -13,3 +13,59 @@ class Scenario(models.Model):
 
     def __str__(self):
         return self.scenario_name
+#
+# class Teacher(models.Model):
+#
+#
+# class Student(models.Model):
+#
+#
+# class Chat(models.Model):
+#
+#
+#
+# class Quiz(models.Model):
+#
+#
+#
+#
+# class Transcript(model.Models):
+
+
+
+# from django.db import models
+# from django.contrib.postgres.fields import ArrayField
+#
+# # Create your models here.
+# class Scenario(models.Model):
+#     scenario_name = models.TextField()
+#
+#     def __str__(self):
+#         return self.scenario_name
+#
+# class Teacher(models.Model):
+#     scenario = models.ForeignKey(Scenario)
+#     background = models.TextField()
+#     objective = models.TextField()
+#     hints = ArrayField(models.TextField())
+#
+# class Student(models.Model):
+#     scenario = models.ForeignKey(Scenario)
+#     background = models.TextField()
+#     profile = models.TextField()
+#     hints = ArrayField(models.TextField())
+#
+# class Observer(models.Model):
+#
+# class Question(models.Model):
+#     question = models.TextField()
+#     answers = models.
+#     answer =
+#     image =
+#
+# class Chatroom(models.Model):
+#     scenario =
+#     participants =
+#
+# class Transcript(models.Model):
+    #     scenario = models.ForeignKey(Chatroom)
