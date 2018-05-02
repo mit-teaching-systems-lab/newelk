0139be86a97ff135b0c17f223b89c21eb2affbd7
Brandon <bhanks@mit.edu>
2018-05-02 14:56:49 -0400

    adding scenarios to navigation

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
+from .models import Scenario
 
 def index(request):
-    return render(request, 'chat/index.html', {})
+    scenarios = Scenario.objects.all()
+    return render(request, 'chat/index.html', {'scenarios': scenarios})
 
 def room(request, room_name):
+
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
