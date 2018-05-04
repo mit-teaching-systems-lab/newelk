cf0c11f88d1f63426d327e47ef76eb4a6bcc31e4
Brandon <bhanks@mit.edu>
2018-05-03 21:53:53 -0400

    added roles and scenarios to chat room

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='chat_select'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    url(r'^(?P<role>[-\s\w]+)/(?P<scenario>[-\s\w]+)/(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -7,8 +7,27 @@ def index(request):
     scenarios = Scenario.objects.all()
     return render(request, 'chat/index.html', {'scenarios': scenarios})
 
-def room(request, room_name):
+def room(request, role, scenario, room_name):
+    scene = Scenario.objects.filter(pk=scenario)
+    print(scene)
+    room_details = {
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'scenario_name': scene.scenario_name,
+        'student_background': scene.student_background,
+        'student_profile': scene.student_profile,
+        'student_hints': scene.student_hints,
+        'teacher_background': scene.teacher_background,
+        'teacher_objective': scene.teacher_objective,
+        'teacher_hints': scene.teacher_hints,
+    }
+    if role=='s':
+        room_details.pop('teacher_background', None)
+        room_details.pop('teacher_objective', None)
+        room_details.pop('teacher_hints', None)
 
-    return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
-    })
+    if role=='t':
+        room_details.pop('student_background', None)
+        room_details.pop('student_profile', None)
+        room_details.pop('student_hints', None)
+
+    return render(request, 'chat/room.html', room_details)
