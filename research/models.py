37a0e6cee887065890f80c75604a43df923d5415
Brandon <bhanks@mit.edu>
2018-05-04 11:41:16 -0400

    change many-to-many to foreign key for user transcript

--- research/models.py
+++ research/models.py
@@ -5,11 +5,14 @@ from django.utils import timezone
 
 # Create your models here.
 class Transcript(models.Model):
-    users = models.ManyToManyField(User)
+    # users = models.ManyToManyField(User)
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     transcript = models.TextField(default="")
     room_name = models.TextField()
     creation_time = models.DateTimeField(default=timezone.now)
     # teacher_hints = ArrayField(models.TextField())
+    def __str__(self):
+        return self.users + self.creation_time
 
 
 # class Score(models.Model):
