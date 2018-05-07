14bcaa425dea9d8105b21cbf387c16bd93727f40
Brandon <bhanks@mit.edu>
2018-05-07 19:32:57 -0400

    added transcript to profile page

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,6 @@ from research.models import Transcript
 # Create your views here.
 def profile(request):
     transcripts = Transcript.objects.filter(users=request.user).order_by("creation_time")
-
+    # scores =
 
     return render(request, 'profile.html',{"transcripts": transcripts})
