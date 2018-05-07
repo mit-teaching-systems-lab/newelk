01d9723cea1df4be356023d98d36e9f719aa44d1
Brandon <bhanks@mit.edu>
2018-05-07 19:18:54 -0400

    added transcript query to profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from research.models import Transcript
+
 
 # Create your views here.
 def profile(request):
+    transcripts = Transcript.objects.filter(users=request.user).order_by("creation_time")
 
 
-    return render(request, 'profile.html')
+    return render(request, 'profile.html',{"transcripts": transcripts})
