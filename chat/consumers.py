cab0443cbcbe8e08c370590cf2b40d6dc68180b5
Brandon <bhanks@mit.edu>
2018-05-07 11:16:26 -0400

    added role and scenario info to chat channel

--- chat/consumers.py
+++ chat/consumers.py
@@ -20,6 +20,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             # print(value)
 
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.
         self.room_group_name = 'chat_%s' % self.room_name
 
         self.room = ChatRoom.objects.filter(name=self.room_name)
