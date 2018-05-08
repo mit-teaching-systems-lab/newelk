4ad65e573c79ac4d9d47366c67766b498401999c
Brandon <bhanks@mit.edu>
2018-05-07 20:38:49 -0400

    troubleshooting

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ def profile(request):
     transcripts = Transcript.objects.filter(users=request.user).order_by("creation_time")
     latest_transcript = transcripts.latest("creation_time")
 
-    answers = TFAnswer.objects.filter(transcript=latest_transcript.pk,users=latest_transcript.users)
+    # answers = TFAnswer.objects.filter(transcript=latest_transcript.pk,users=latest_transcript.users)
 
 
     # for user in latest_transcript.users:
