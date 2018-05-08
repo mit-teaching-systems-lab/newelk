9546528ccc337016d77d0cf194e23f3487462d8b
Brandon <bhanks@mit.edu>
2018-05-07 21:33:47 -0400

    added quiz results to view

--- accounts/views.py
+++ accounts/views.py
@@ -6,11 +6,14 @@ from research.models import Transcript, TFAnswer
 def profile(request):
     transcripts = Transcript.objects.filter(users=request.user).order_by("creation_time")
     latest_transcript = transcripts.latest("creation_time")
-
-    # answers = TFAnswer.objects.filter(transcript=latest_transcript.pk,users=latest_transcript.users)
-
-
-    # for user in latest_transcript.users:
-
-    # , "answers": answers
-    return render(request, 'profile.html',{"transcripts": transcripts})
+    participants = latest_transcript.users.distinct()
+    quiz_results = {}
+    for person in participants:
+        answers = TFAnswer.objects.filter(transcript=latest_transcript,user=person)
+        quiz_results[person.username] = {}
+        quiz_results["correct_answer"] = {}
+        for answer in answers:
+            quiz_results[person.username][answer.pk] = answer.user_answer
+            quiz_results["correct_answer"][answer.pk] = answer.correct_answer
+    
+    return render(request, 'profile.html',{"transcripts": transcripts, "quiz_results": quiz_results})
