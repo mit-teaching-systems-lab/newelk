f238e7e7ad931291b3954cbd8065caac1b23c89f
Brandon <bhanks@mit.edu>
2018-05-08 15:48:20 -0400

    trying to fix quiz result filter

--- accounts/views.py
+++ accounts/views.py
@@ -9,9 +9,11 @@ def profile(request):
     transcripts = Transcript.objects.filter(users=request.user).order_by("-creation_time")
     latest_transcript = transcripts.latest("creation_time")
     participants = latest_transcript.users.distinct()
+    scenario = latest_transcript.scene
     quiz_results = {}
     for person in participants:
-        answers = TFAnswer.objects.filter(transcript=latest_transcript,user=person)
+        answers = TFAnswer.objects.filter(question__scenario=scenario, user=person)
+        # answers = TFAnswer.objects.filter(transcript=latest_transcript,user=person)
         quiz_results[person.username] = {}
         quiz_results["correct_answer"] = {}
         for answer in answers:
