81de7d8a17b4e410b8bae79f95179ecac74f605d
Brandon <bhanks@mit.edu>
2018-05-24 16:26:35 -0400

    added a simple header

--- research/views.py
+++ research/views.py
@@ -15,7 +15,13 @@ class Echo:
     """An object that implements just the write method of the file-like
     interface.
     """
+    def __init__(self, column_headers):
+        self.header = column_headers
+        self.header_written = False
     def write(self, value):
+        if not self.header_written:
+            value = self.header + '\n' + value
+            self.header_written = True
         """Write the value by returning it, instead of storing in a buffer."""
         return str(value)
 
@@ -26,10 +32,8 @@ def streaming_csv_view(request):
     # applications.
     # rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
     rows = Message.objects.all().order_by("transcript", "-creation_time")
-    pseudo_buffer = Echo()
-    # writer = csv.writer(pseudo_buffer)
-    # response = StreamingHttpResponse((writer.writerow(str(row)) for row in rows),
-    #                                  content_type="text/csv")
+    headers = "group_id,room_name,username,role,message_id,message_text,time"
+    pseudo_buffer = Echo(headers)
     response = StreamingHttpResponse((pseudo_buffer.write(row) + '\n' for row in rows),
                                      content_type="text/csv")
     response['Content-Disposition'] = 'attachment; filename="data.csv"'
