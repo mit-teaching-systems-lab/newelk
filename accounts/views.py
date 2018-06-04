5a3e85c7d525d00afec7462654c2ecc87c33758b
Brandon <bhanks@mit.edu>
2018-06-03 21:40:55 -0400

    fixing when no transcripts for profile

--- accounts/views.py
+++ accounts/views.py
@@ -7,19 +7,21 @@ def profile(request):
     if not request.user.is_authenticated:
         return redirect('/accounts/login/')
     transcripts = Transcript.objects.filter(users=request.user).order_by("-creation_time")
-    for t in transcripts:
-        t.messages = Message.objects.filter(transcript=t).order_by("-creation_time")
-    latest_transcript = transcripts.latest("creation_time")
-    participants = latest_transcript.users.distinct()
-    scenario = latest_transcript.scenario
     quiz_results = {}
-    for person in participants:
-        answers = TFAnswer.objects.filter(question__scenario=scenario, user=person, transcript=latest_transcript)
-        # answers = TFAnswer.objects.filter(transcript=latest_transcript,user=person)
-        quiz_results[person.username] = {}
-        quiz_results["question_details"] = {}
-        for answer in answers:
-            quiz_results[person.username][answer.pk] = answer.user_answer
-            quiz_results["question_details"][answer.pk] = { answer.question.question : answer.correct_answer }
-
+    if transcripts:
+            for t in transcripts:
+                t.messages = Message.objects.filter(transcript=t).order_by("-creation_time")
+            latest_transcript = transcripts.latest("creation_time")
+            participants = latest_transcript.users.distinct()
+            scenario = latest_transcript.scenario
+            for person in participants:
+                answers = TFAnswer.objects.filter(question__scenario=scenario, user=person, transcript=latest_transcript)
+                # answers = TFAnswer.objects.filter(transcript=latest_transcript,user=person)
+                quiz_results[person.username] = {}
+                quiz_results["question_details"] = {}
+                for answer in answers:
+                    quiz_results[person.username][answer.pk] = answer.user_answer
+                    quiz_results["question_details"][answer.pk] = { answer.question.question : answer.correct_answer }
+    else:
+        transcripts = None
     return render(request, 'profile.html',{"transcripts": transcripts, "quiz_results": quiz_results})
