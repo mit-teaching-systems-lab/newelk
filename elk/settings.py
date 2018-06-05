a6d7a4578ac98de8167e83734a79fb8ab85af681
Brandon <bhanks@mit.edu>
2018-06-05 14:36:29 -0400

    still troubleshooting duplicate messages

--- elk/settings.py
+++ elk/settings.py
@@ -171,7 +171,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
         },
     },
 }
