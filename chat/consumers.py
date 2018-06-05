2b90a828127efb9bffba9a8bc69ef76633619d8c
Brandon <bhanks@mit.edu>
2018-06-05 13:12:26 -0400

    bugfixing duplicate messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -89,7 +89,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         if self.room.transcript.last_line != message:
             self.room.transcript.last_line = message
             self.room.transcript.save()
-            print(self.room.transcript.last_line)
+            sys.stdout.write(self.room.transcript.last_line)
             self.msg_obj = Message(text=message, user=self.user, role=self.role, transcript=self.room.transcript)
             self.msg_obj.save()
 
