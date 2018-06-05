84d69f8e704a8933f2328f386620d676d10352cb
Brandon <bhanks@mit.edu>
2018-06-05 15:02:05 -0400

    fix for answers with missing data

--- research/models.py
+++ research/models.py
@@ -28,9 +28,9 @@ class TFAnswer(models.Model):
     correct_answer = models.TextField(null=True)
     creation_time = models.DateTimeField(default=timezone.now)
     def __str__(self):
-        transcript_id = str(self.transcript.id) if self.transcript.id else 'no_transcript_id'
+        transcript_id = str(self.transcript.id) if self.transcript else 'no_transcript_id'
         username = self.user.username if self.user else 'System'
-        question_id = str(self.question.id) if str(self.question.id) else 'no_question_id'
+        question_id = str(self.question.id) if self.question else 'no_question_id'
         question = self.question.question if self.question else 'no_question'
         correct_answer = str(self.question.answer) if str(self.question.answer) else 'no_question_answer'
         user_response = self.user_answer if self.user_answer else 'no_user_answer'
