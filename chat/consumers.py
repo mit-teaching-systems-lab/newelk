9f25260c5ff1f51c103fc8f4ccbe14279f94e005
Brandon <bhanks@mit.edu>
2018-07-07 23:19:27 -0400

    added checking for websocket message presence

--- chat/consumers.py
+++ chat/consumers.py
@@ -76,25 +76,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = self.user.username if self.user.username != "" else "Anonymous"
         if 'message' in text_data_json:
             message =  username + ": " + text_data_json['message']
-        # else:
-            
-        # if 'ready' in text_data_json:
+            logger.info(self.room.transcript.last_line)
+            logger.info(message)
+            if self.room.transcript.last_line != message:
+                msg_obj = Message(text=message, user=self.user, role=self.role, transcript=self.room.transcript)
+                msg_obj.save()
+                self.room.transcript.last_line = message
 
-
-        logger.info(self.room.transcript.last_line)
-        logger.info(message)
-        if self.room.transcript.last_line != message:
-            msg_obj = Message(text=message, user=self.user, role=self.role, transcript=self.room.transcript)
-            msg_obj.save()
-            self.room.transcript.last_line = message
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message
+                }
+            )
 
     # Receive message from room group
     async def chat_message(self, event):
