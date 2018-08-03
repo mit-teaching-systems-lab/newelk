95118dd5e97ef5780d0283287d1693b770c53b1f
Brandon <bhanks@mit.edu>
2018-08-03 15:52:41 -0400

    still learning about model admin

--- chat/admin.py
+++ chat/admin.py
@@ -4,11 +4,14 @@ from .models import Scenario, TFQuestion, ChatRoom
 class ScenarioAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         # if change:
+            newobj =
         print(str(change))
         print('in admin')
-        print(self)
         print(request)
         print(obj)
+        print(obj.pk)
+        print(type(form))
+        print(form.changed_data)
         for x in form:
             print(x)
         # super.save_model(request, obj, form, change)
