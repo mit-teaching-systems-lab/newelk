7ee15e11c2184b706e4fbc30f3bcf9dd9be43558
Brandon <bhanks@mit.edu>
2018-08-03 17:22:11 -0400

    reenabled scenario saving

--- chat/admin.py
+++ chat/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
 from .models import Scenario, TFQuestion, ChatRoom
 
-class ScenarioAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        # if change:
-        #     newobj =
-        print(str(change))
-        print('in admin')
-        print(request)
-        print(obj)
-        print(obj.pk)
-        print(type(form))
-        print(form.changed_data)
-        for k, v in obj:
-            print(k)
-            print(v)
+# class ScenarioAdmin(admin.ModelAdmin):
+#     def save_model(self, request, obj, form, change):
+#         # if change:
+#         #     newobj =
+#         print(str(change))
+#         print('in admin')
+#         print(request)
+#         print(obj)
+#         print(obj.pk)
+#         print(type(form))
+#         print(form.changed_data)
+#         for k, v in obj:
+#             print(k)
+#             print(v)
         # for x in form:
         #     print(x)
         # super.save_model(request, obj, form, change)
 
 admin.site.register(ChatRoom)
-admin.site.register(Scenario, ScenarioAdmin)
+# admin.site.register(Scenario, ScenarioAdmin)
+admin.site.register(Scenario)
 admin.site.register(TFQuestion)
