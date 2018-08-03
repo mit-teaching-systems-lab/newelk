27ed9fd458822eab8fe4d26988344fe9787c67cd
Brandon <bhanks@mit.edu>
2018-08-03 15:27:33 -0400

    added messages to guide new scene editor

--- chat/admin.py
+++ chat/admin.py
@@ -3,12 +3,13 @@ from .models import Scenario, TFQuestion, ChatRoom
 
 class ScenarioAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
+        print('in admin')
         print(self)
         print(request)
         print(obj)
         print(form)
         print(change)
-        super.save_model(request, obj, form, change)
+        # super.save_model(request, obj, form, change)
 
 admin.site.register(ChatRoom)
 admin.site.register(Scenario, ScenarioAdmin)
