582d6e646ed9abce8a511180cb5dc5b76a836b97
Brandon <bhanks@mit.edu>
2018-08-07 16:52:25 -0400

    added the custom admin to the scenario editor

--- chat/admin.py
+++ chat/admin.py
@@ -4,14 +4,17 @@ from .models import Scenario, TFQuestion, ChatRoom
 class ScenarioAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if change:
+            print('new scenario')
             obj.visible_to_players = False
             obj.save()
+            print(obj.pk)
             obj.pk = None
             obj.save()
+            print(obj.pk)
 
         # super.save_model(request, obj, form, change)
 
 admin.site.register(ChatRoom)
-# admin.site.register(Scenario, ScenarioAdmin)
-admin.site.register(Scenario)
+admin.site.register(Scenario, ScenarioAdmin)
+# admin.site.register(Scenario)
 admin.site.register(TFQuestion)
