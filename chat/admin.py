329c8cb7afae95475d3b7cb040941f74f9961f2c
Brandon <bhanks@mit.edu>
2018-08-15 15:55:56 -0400

    testing scene editor

--- chat/admin.py
+++ chat/admin.py
@@ -6,29 +6,8 @@ class NonStaffAdmin(AdminSite):
     def has_permission(self, request):
         return request.user.is_active
 
-nonstaff_admin_site = NonStaffAdmin()
-
-class ScenarioAdmin(admin.ModelAdmin):
-    readonly_fields = ('creation_time',)
-    save_as = True
-    def save_model(self, request, obj, form, change):
-        if change:
-            # print('new scenario')
-            obj.visible_to_players = False
-            obj.save()
-            # print(obj.pk)
-            previous_pk = obj.pk
-            obj.pk = None
-            obj.previous_verion = Scenario.objects.get(pk=previous_pk)
-            obj.save()
-            # print(obj.pk)
+nonstaff_admin_site = NonStaffAdmin(name='nonstaffadmin')
 
 nonstaff_admin_site.register(ChatRoom)
 nonstaff_admin_site.register(Scenario)
 nonstaff_admin_site.register(TFQuestion)
-
-#
-# admin.site.register(ChatRoom)
-# admin.site.register(Scenario, ScenarioAdmin)
-# # admin.site.register(Scenario)
-# admin.site.register(TFQuestion)
