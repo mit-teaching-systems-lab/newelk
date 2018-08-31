a1aa9afbdf0ddefd2e7825493c99df4d625a3a79
Brandon <bhanks@mit.edu>
2018-08-31 14:39:30 -0400

    testing lowercase users, still need to force lowercase in acct creation though

--- research/models.py
+++ research/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from accounts.models import ELKUser as User
 from chat.models import Scenario
 from django.utils import timezone
 
