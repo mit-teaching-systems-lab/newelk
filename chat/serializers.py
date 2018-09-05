a1064c44a3e7816dedc7114240e180d5c0c29b1a
Brandon <bhanks@mit.edu>
2018-09-05 17:20:36 -0400

    refining chat api

--- chat/serializers.py
+++ chat/serializers.py
@@ -6,4 +6,4 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatRoom
         # fields = ('__all__')
-        fields = ('name')
+        fields = ('name',)
