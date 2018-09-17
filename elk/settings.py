9f6f586f4099a038a671999c681b83ee239ea5a0
Brandon <bhanks@mit.edu>
2018-09-17 19:29:51 -0400

    fixing dirs

--- elk/settings.py
+++ elk/settings.py
@@ -86,7 +86,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'global_templates'),
-                 os.path.join(BASE_DIR, 'accounts/templates/allauth/templates')],
+                 os.path.join(BASE_DIR, 'accounts/templates/allauth/templates'),
+                 os.path.join(BASE_DIR, 'accounts/templates/allauth/templates/account/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
