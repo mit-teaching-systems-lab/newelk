0c15ccd12318e29ba394ce0bf003d7b5296df043
Brandon <bhanks@mit.edu>
2018-09-17 13:42:19 -0400

    adding email settings

--- elk/settings.py
+++ elk/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'anymail',
 ]
 
 SITE_ID = 1
@@ -189,22 +190,18 @@ STATIC_URL = '/static/'
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
-DEFAULT_FROM_EMAIL = env(
-    'DJANGO_DEFAULT_FROM_EMAIL',
-    default='ELK Admin <bhanks@mit.edu>'
-)
+DEFAULT_FROM_EMAIL = 'ELK Admin <bhanks@mit.edu>'
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
+SERVER_EMAIL = 'ELK Admin <bhanks@mit.edu>'
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[Elk]')
+EMAIL_SUBJECT_PREFIX = '[Elk]'
 
 # Anymail (Mailgun)
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-INSTALLED_APPS += ['anymail']  # noqa F405
 EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 ANYMAIL = {
-    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
+    'MAILGUN_API_KEY': os.environ['MAILGUN_API_KEY'],
+    'MAILGUN_SENDER_DOMAIN': os.environ['MAILGUN_DOMAIN']
 }
