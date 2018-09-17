4b831aca1ad1be6a0eb8e16b285e0f98a972b1af
Brandon <bhanks@mit.edu>
2018-09-17 12:02:17 -0400

    url testing

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')),
+    path('/', include('allauth.urls')),
     path('profile/', views.profile, name='profile'),
     path('about/', views.about, name='about')
 ]
