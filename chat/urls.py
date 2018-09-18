fcf92de3893cc01a24bccf9b6511926cc989f74f
Brandon <bhanks@mit.edu>
2018-09-17 22:33:11 -0400

    url fix

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='chat_select'),
+    url(r'^$', views.select_role, name='select_role'),
+    path('/t', views.join_scenario, name='join_scenario'),
+    path('/s', views.select_scenario, name='select_scenario'),
     url(r'^(?P<role>[-\s\w]+)/(?P<scenario>[-\s\w]+)/(?P<room_name>[^/]+)/$', views.room, name='room'),
     url(r'^(?P<role>[-\s\w]+)/(?P<scenario>[-\s\w]+)/(?P<room_name>[^/]+)/quiz/$', views.quiz, name='quiz'),
 ]
