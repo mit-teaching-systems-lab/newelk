99cc87a14db1914c375257f06a37222e01b61649
Brandon <bhanks@mit.edu>
2018-10-05 14:23:51 -0400

    added template & form for scene editor

---
+++ chat/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+BOOL_CHOICES = ((True, 'True'), (False, 'False'))
+
+class ContactForm(forms.Form):
+    scenario_name = forms.CharField(max_length=50)
+    student_background = forms.TextField()
+    student_profile = forms.TextField()
+    teacher_background = forms.TextField()
+    teacher_objective = forms.TextField()
+    visible_to_players = forms.BooleanField(
+        choices=BOOL_CHOICES,
+        default=True,
+    )
