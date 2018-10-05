d9b25146b3bcd0ea245ba29f3d5b1d50a7ba02bb
Brandon <bhanks@mit.edu>
2018-10-05 14:43:27 -0400

    added scene editor url

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('scenarios/chat/scenario/<int:pk>/change/', views.scenario_editor, name='scenario_editor'),
     url(r'^$', views.select_role, name='select_role'),
     path('t/', views.join_scenario, name='join_scenario'),
     path('s/', views.select_scenario, name='select_scenario'),
