792bbb108fc88abe093f930dcb14e05f2612a427
Brandon <bhanks@mit.edu>
2018-10-19 14:02:45 -0400

    debugging scenario editor

--- chat/forms.py
+++ chat/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 BOOL_CHOICES = ((True, 'True'), (False, 'False'))
 
-class ScenarioForm(forms.Form):
+class ScenarioForm(forms.ModelForm):
     scenario_name = forms.CharField(max_length=50)
     student_background = forms.CharField(widget=forms.Textarea)
     student_profile = forms.CharField(widget=forms.Textarea)
