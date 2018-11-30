b611b20b497c4e1de59b3386330e712882114137
Brandon <bhanks@mit.edu>
2018-11-30 15:40:06 -0500

    username ref

--- research/serializers.py
+++ research/serializers.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 class TFAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = TFAnswer
-        user_name = serializers.CharField(source='user.name')
-        fields = ('user_name','question','user_answer','correct_answer')
+        # user_name = serializers.CharField(source='user.name')
+        fields = ('user.name','question','user_answer','correct_answer')
