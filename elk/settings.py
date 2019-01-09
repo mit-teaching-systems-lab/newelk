db79ac3d37ceaa6dfba71ddefa34fd1ed44df512
Brandon <bhanks@mit.edu>
2019-01-09 16:47:39 -0500

    added assets

--- elk/settings.py
+++ elk/settings.py
@@ -190,10 +190,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # EMAIL
 # ------------------------------------------------------------------------------
